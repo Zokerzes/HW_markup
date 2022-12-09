5a1063cc47db4f36087d8fcda9aa35cbe41255f3
Zokerzes <Bliz_mw86@itstepru.onmicrosoft.com>
2022-12-09 19:40:02 +0300

    ++классы user и usersDB

---
+++ HW_markup/Model/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_markup.Model
+{
+    internal class User
+    {
+        public User(string name, string login, string password)
+        {
+            Name = name;
+            Login = login;
+            Password = password;
+        }
+        public string Name { get; private set; }
+        public string Login { get; private set; }
+        string Password { get; set; }
+
+        public bool isAutorisation(string password) => password == Password;
+
+
+    }
+}
---
+++ HW_markup/Model/UsersDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_markup.Model
+{
+    internal class UsersDB
+    {
+        
+        public List<User> Users { get; set; } = new List<User>()
+        {
+            new User("Вася","user111","123"),
+            new User("Петя","user222","234"),
+            new User("Коля","user333","345"),
+            new User("Ваня","user444","456"),
+            new User("Маша","user555","567"),         //вместо конструктора
+        };
+    }
+}

038ad63e0be1d015a0b72cab1cd8aff02e65188a
Zokerzes <Bliz_mw86@itstepru.onmicrosoft.com>
2022-12-09 19:06:53 +0300

    ++

--- HW_markup/Window1.xaml.cs
+++ HW_markup/Window1.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace HW_markup
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Window auth = new Window1();
-            auth.ShowDialog();
         }
     }
 }

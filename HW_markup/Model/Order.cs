ea26c998d6bdc4809d285903f02505096cb83195
Zokerzes <Bliz_mw86@itstepru.onmicrosoft.com>
2022-12-14 21:21:39 +0300

    product order

---
+++ HW_markup/Model/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_markup.Model
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+
+        public string Client { get; set; }
+        public List<Product> Products { get; set; }
+        public decimal Price { get { return Products.Sum(x => x.Price);}
+
+    }
+}
